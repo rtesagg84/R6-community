9e5f79640c127dacbb655bb20800c025eb7fa6ce
rtesagg84 <rtesagg84@gmail.com>
2021-12-09 17:03:41 -0500

    complit all the rest  steps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_213953) do
+ActiveRecord::Schema.define(version: 2021_12_09_215757) do
 
   create_table "forums", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_12_09_213953) do
     t.integer "forum_id", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "twitter"
+    t.string "github"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_12_09_213953) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
